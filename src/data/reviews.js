344cae4d5a00d6ac0f10b47f043df60a56da65c4
RandomMoh <mohsinddop2309@gmail.com>
2026-07-03 06:02:42 +0900

    style: polish video review titles

--- src/data/reviews.js
+++ src/data/reviews.js
@@ -1,20 +1,20 @@
 export const academyReviews = [
-  { id: '1wcUhBwAZiJZdbG9q-kXrBNBX7K9cDPcC', title: '1st Review' },
-  { id: '17ouS8_0ucnxOA8wm1RinrSeWhgx45dHv', title: '2nd Review' },
-  { id: '1dJBJzNC7hTCVyeFrhcmTB19a8G7S1pnT', title: '3rd Review' },
-  { id: '1cCRySVzkeyaRiQ0anBi06iNmu6nUQeky', title: '4th Review' },
-  { id: '11DwAH36h0OJ5Au42gYLGwYx7KbxtDLUj', title: '5th Review' }
+  { id: '1wcUhBwAZiJZdbG9q-kXrBNBX7K9cDPcC', title: 'Student Review 1' },
+  { id: '17ouS8_0ucnxOA8wm1RinrSeWhgx45dHv', title: 'Student Review 2' },
+  { id: '1dJBJzNC7hTCVyeFrhcmTB19a8G7S1pnT', title: 'Student Review 3' },
+  { id: '1cCRySVzkeyaRiQ0anBi06iNmu6nUQeky', title: 'Student Review 4' },
+  { id: '11DwAH36h0OJ5Au42gYLGwYx7KbxtDLUj', title: 'Student Review 5' }
 ];
 
 export const instituteReviews = [
-  { id: '1y-FiFSkXJdtOcOTI2VcPS0-2-3J2LXx5', title: 'FEEDBACK 2' },
-  { id: '1v0WeyW_X8CcZeVLWKaJvlpm8uooVPkNC', title: 'Beauty course Feedback girl 1' },
-  { id: '1uQ0qXq1sRHTItAp25sOTx6zP5WYPvWYU', title: 'AD 8' },
-  { id: '1plbZdphgT_X85MNAygwG7gEh1D-DVZp6', title: 'Autocad Feedback Video' },
-  { id: '1lXi-jmFJ5ZTaamMcnzId5_FlH-a9819v', title: 'Autocad boy feedback video' },
-  { id: '1jQ1dXveGRFbgtZYvSbuaXGzAQjwXEKqs', title: 'Student Feedback 5' },
-  { id: '1gaQo6hY5uzeOWb97-_6a_ggezuO4Rl-4', title: 'Beauty course Feedback girl 2' },
-  { id: '1KIVE4O6b1WUVml8uRLwRBmNrXi1wpsNn', title: 'Feedback Video 1' },
-  { id: '1CFjRmHJ_7f52c-U2_j9jNBat7Ih8gXkt', title: 'Feedback Video 3' },
-  { id: '1C0_G2DTJdQPpAmoatJiVFjTwd75sFnUu', title: 'AD 3' }
+  { id: '1y-FiFSkXJdtOcOTI2VcPS0-2-3J2LXx5', title: 'Student Feedback 1' },
+  { id: '1v0WeyW_X8CcZeVLWKaJvlpm8uooVPkNC', title: 'Beauty Course Feedback' },
+  { id: '1uQ0qXq1sRHTItAp25sOTx6zP5WYPvWYU', title: 'Student Story' },
+  { id: '1plbZdphgT_X85MNAygwG7gEh1D-DVZp6', title: 'AutoCAD Feedback' },
+  { id: '1lXi-jmFJ5ZTaamMcnzId5_FlH-a9819v', title: 'AutoCAD Student Story' },
+  { id: '1jQ1dXveGRFbgtZYvSbuaXGzAQjwXEKqs', title: 'Student Feedback 2' },
+  { id: '1gaQo6hY5uzeOWb97-_6a_ggezuO4Rl-4', title: 'Beauty Course Experience' },
+  { id: '1KIVE4O6b1WUVml8uRLwRBmNrXi1wpsNn', title: 'Student Feedback 3' },
+  { id: '1CFjRmHJ_7f52c-U2_j9jNBat7Ih8gXkt', title: 'Student Feedback 4' },
+  { id: '1C0_G2DTJdQPpAmoatJiVFjTwd75sFnUu', title: 'Student Journey' }
 ];
